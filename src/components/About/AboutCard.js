832e6d2748e517d44f0601b1d041d7e08558a78f
Nafyad Hailu <nafyadhailu-ug.aau.edu.et>
2024-06-29 20:26:02 +0300

    add about page

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Nafyad Hailu </span>
+            from <span className="purple"> Addis Ababa ,Ethiopia.</span>
+            <br />
+           
+            I am pursuing a BSc in Computer Science at Addis Ababa University.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching Movies and Series
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading psychology books
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Nafyad</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
