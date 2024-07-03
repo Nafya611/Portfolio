29d0dbce4101cdea1e490e65b91831b583f38c03
Nafyad Hailu <nafyadhailu-ug.aau.edu.et>
2024-07-03 03:40:08 +0300

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Developed by Nafyad Hailu</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} NH</h3>
+          <h3>Copyright © {year} Nh</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
